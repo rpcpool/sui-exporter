9737c53836164bb68cef5939702df3cad2122907
linuskendall <git@linuskendall.com>
2023-01-26 15:59:50 +0000

    Add basic exporter setup

---
+++ cmd/sui_exporter/exporter.go
@@ -0,0 +1,186 @@
+package main
+
+import (
+	"context"
+	"log"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/ybbus/jsonrpc/v3"
+)
+
+type Exporter struct {
+	state          *SuiSystemState
+	epoch          string
+	rpcClient      jsonrpc.RPCClient
+	frequency      time.Duration
+	nodeStateMutex sync.RWMutex
+
+	referenceGasPriceDesc          *prometheus.Desc
+	epochStartTimestampMs          *prometheus.Desc
+	storageFundBalance             *prometheus.Desc
+	stakeSubsidyBalance            *prometheus.Desc
+	stakeSubsidyCurrentEpochAmount *prometheus.Desc
+	totalValidatorStake            *prometheus.Desc
+	totalDelegationStake           *prometheus.Desc
+	validatorVotingPower           *prometheus.Desc
+	validatorCommission            *prometheus.Desc
+	validatorGasPrice              *prometheus.Desc
+	validatorPendingStake          *prometheus.Desc
+	validatorPendingWithdrawal     *prometheus.Desc
+	validatorStake                 *prometheus.Desc
+	validatorDelegationBalance     *prometheus.Desc
+	validatorNextEpochStake        *prometheus.Desc
+	validatorNextEpochDelegation   *prometheus.Desc
+	validatorNextEpochGasPrice     *prometheus.Desc
+	validatorNextEpochCommission   *prometheus.Desc
+}
+
+func NewExporter(uri string, frequency int) *Exporter {
+	return &Exporter{
+		state:     nil,
+		rpcClient: jsonrpc.NewClient(uri),
+		frequency: time.Duration(frequency),
+		referenceGasPriceDesc: prometheus.NewDesc(
+			"sui_reference_gas_price",
+			"Information about gas price",
+			[]string{"epoch"}, nil,
+		),
+		epochStartTimestampMs: prometheus.NewDesc(
+			"sui_epoch_start_timestamp_ms",
+			"Information about epoch start timestamp",
+			[]string{"epoch"}, nil,
+		),
+		storageFundBalance: prometheus.NewDesc(
+			"sui_storage_fund_balance",
+			"Storage fund balance",
+			[]string{"epoch"}, nil,
+		),
+		stakeSubsidyBalance: prometheus.NewDesc(
+			"sui_stake_subsidy_balance",
+			"Stake subsidy balance",
+			[]string{"epoch"}, nil,
+		),
+		stakeSubsidyCurrentEpochAmount: prometheus.NewDesc(
+			"sui_stake_subsidy_current_epoch_amount",
+			"Stake subsidy current epoch amount",
+			[]string{"epoch"}, nil,
+		),
+		totalValidatorStake: prometheus.NewDesc(
+			"sui_total_validator_stake",
+			"Total validator stake",
+			[]string{"epoch"}, nil,
+		),
+		totalDelegationStake: prometheus.NewDesc(
+			"sui_total_delegation_stake",
+			"Total delegation stake",
+			[]string{"epoch"}, nil,
+		),
+		validatorVotingPower: prometheus.NewDesc(
+			"sui_validator_voting_power",
+			"Validator voting power",
+			[]string{"epoch", "address", "name"}, nil,
+		),
+		validatorCommission: prometheus.NewDesc(
+			"sui_validator_commission",
+			"Validator commission",
+			[]string{"epoch", "address", "name"}, nil,
+		),
+		validatorGasPrice: prometheus.NewDesc(
+			"sui_validator_gas_price",
+			"Validator gas price",
+			[]string{"epoch", "address", "name"}, nil,
+		),
+		validatorPendingStake: prometheus.NewDesc(
+			"sui_validator_pending_stake",
+			"Validator pending stake",
+			[]string{"epoch", "address", "name"}, nil,
+		),
+		validatorPendingWithdrawal: prometheus.NewDesc(
+			"sui_validator_pending_withdrawal",
+			"Validator pending withdrawal",
+			[]string{"epoch", "address", "name"}, nil,
+		),
+		validatorStake: prometheus.NewDesc(
+			"sui_validator_stake",
+			"Validator stake",
+			[]string{"epoch", "address", "name"}, nil,
+		),
+		validatorDelegationBalance: prometheus.NewDesc(
+			"sui_validator_delegation_balance",
+			"Validator delegation balance",
+			[]string{"epoch", "address", "name"}, nil,
+		),
+		validatorNextEpochStake: prometheus.NewDesc(
+			"sui_validator_next_epoch_stake",
+			"Validator next epoch stake",
+			[]string{"epoch", "address", "name"}, nil,
+		),
+		validatorNextEpochDelegation: prometheus.NewDesc(
+			"sui_validator_next_epoch_delegation",
+			"Validator next epoch delegation",
+			[]string{"epoch", "address", "name"}, nil,
+		),
+		validatorNextEpochGasPrice: prometheus.NewDesc(
+			"sui_validator_next_epoch_gas_price",
+			"Validator next epoch gas price",
+			[]string{"epoch", "address", "name"}, nil,
+		),
+		validatorNextEpochCommission: prometheus.NewDesc(
+			"sui_validator_next_epoch_commission",
+			"Validator next epoch commission",
+			[]string{"epoch", "address", "name"}, nil,
+		),
+	}
+}
+
+func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
+	ch <- e.referenceGasPriceDesc
+}
+
+func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
+	e.nodeStateMutex.RLock()
+	ch <- prometheus.MustNewConstMetric(e.referenceGasPriceDesc, prometheus.GaugeValue, float64(e.state.ReferenceGasPrice), e.epoch)
+	ch <- prometheus.MustNewConstMetric(e.epochStartTimestampMs, prometheus.GaugeValue, float64(e.state.EpochStartTimestampMs), e.epoch)
+	ch <- prometheus.MustNewConstMetric(e.storageFundBalance, prometheus.GaugeValue, float64(e.state.StorageFundBalance.Value), e.epoch)
+	ch <- prometheus.MustNewConstMetric(e.stakeSubsidyBalance, prometheus.GaugeValue, float64(e.state.StakeSubsidy.Balance.Value), e.epoch)
+	ch <- prometheus.MustNewConstMetric(e.stakeSubsidyCurrentEpochAmount, prometheus.GaugeValue, float64(e.state.StakeSubsidy.CurrentEpochAmount), e.epoch)
+	ch <- prometheus.MustNewConstMetric(e.totalValidatorStake, prometheus.GaugeValue, float64(e.state.Validators.ValidatorStake), e.epoch)
+	ch <- prometheus.MustNewConstMetric(e.totalDelegationStake, prometheus.GaugeValue, float64(e.state.Validators.DelegationStake), e.epoch)
+
+	for _, validator := range e.state.Validators.ActiveValidators {
+		ch <- prometheus.MustNewConstMetric(e.validatorVotingPower, prometheus.GaugeValue, float64(validator.VotingPower), e.epoch, validator.Metadata.SuiAddress, string(validator.Metadata.Name[:]))
+		ch <- prometheus.MustNewConstMetric(e.validatorCommission, prometheus.GaugeValue, float64(validator.CommissionRate), e.epoch, validator.Metadata.SuiAddress, string(validator.Metadata.Name[:]))
+		ch <- prometheus.MustNewConstMetric(e.validatorGasPrice, prometheus.GaugeValue, float64(validator.GasPrice), e.epoch, validator.Metadata.SuiAddress, string(validator.Metadata.Name[:]))
+		ch <- prometheus.MustNewConstMetric(e.validatorPendingStake, prometheus.GaugeValue, float64(validator.PendingStake), e.epoch, validator.Metadata.SuiAddress, string(validator.Metadata.Name[:]))
+		ch <- prometheus.MustNewConstMetric(e.validatorPendingWithdrawal, prometheus.GaugeValue, float64(validator.PendingWithdraw), e.epoch, validator.Metadata.SuiAddress, string(validator.Metadata.Name[:]))
+		ch <- prometheus.MustNewConstMetric(e.validatorStake, prometheus.GaugeValue, float64(validator.StakeAmount), e.epoch, validator.Metadata.SuiAddress, string(validator.Metadata.Name[:]))
+		ch <- prometheus.MustNewConstMetric(e.validatorNextEpochStake, prometheus.GaugeValue, float64(validator.Metadata.NextEpochStake), e.epoch, validator.Metadata.SuiAddress, string(validator.Metadata.Name[:]))
+		ch <- prometheus.MustNewConstMetric(e.validatorNextEpochDelegation, prometheus.GaugeValue, float64(validator.Metadata.NextEpochDelegation), e.epoch, validator.Metadata.SuiAddress, string(validator.Metadata.Name[:]))
+		ch <- prometheus.MustNewConstMetric(e.validatorNextEpochGasPrice, prometheus.GaugeValue, float64(validator.Metadata.NextEpochGasPrice), e.epoch, validator.Metadata.SuiAddress, string(validator.Metadata.Name[:]))
+		ch <- prometheus.MustNewConstMetric(e.validatorNextEpochCommission, prometheus.GaugeValue, float64(validator.Metadata.NextEpochCommissionRate), e.epoch, validator.Metadata.SuiAddress, string(validator.Metadata.Name[:]))
+	}
+
+	e.nodeStateMutex.RUnlock()
+}
+
+func (e *Exporter) WatchState() {
+	ticker := time.NewTicker(e.frequency * time.Second)
+
+	for {
+		e.nodeStateMutex.Lock()
+
+		err := e.rpcClient.CallFor(context.Background(), &e.state, "sui_getSuiSystemState")
+		if err != nil {
+			log.Printf("Error getting node state: %v", err)
+		}
+
+		// Store epoch as string for prometheus labels
+		e.epoch = strconv.Itoa(int(e.state.Epoch))
+
+		e.nodeStateMutex.Unlock()
+		<-ticker.C
+	}
+}
--- cmd/sui_exporter/main.go
+++ cmd/sui_exporter/main.go
@@ -1,19 +1,43 @@
 package main
 
 import (
-	"context"
-	"github.com/ybbus/jsonrpc/v3"
-	"github.com/davecgh/go-spew/spew"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	rpcAddr   = flag.String("rpcURI", "https://testnet.sui.rpcpool.com", "Solana RPC URI (including protocol and path)")
+	addr      = flag.String("addr", ":8080", "Listen address")
+	version   = flag.Bool("version", false, "print version")
+	frequency = flag.Int(("frequency"), 120, "frequency of metrics collection")
 )
 
 func main() {
-	rpcClient := jsonrpc.NewClient("https://testnet.sui.rpcpool.com")
+	log.SetFlags(0)
 
-	var state *SuiSystemState
-	err := rpcClient.CallFor(context.Background(), &state, "sui_getSuiSystemState")
-	if err != nil {
-		spew.Dump(err)
+	flag.Parse()
+
+	if *version {
+		fmt.Println("Version: 0.1")
+		os.Exit(0)
 	}
 
-	spew.Dump(state)
+	if *rpcAddr == "" {
+		log.Fatal("Please specify -rpcURI")
+	}
+
+	exporter := NewExporter(*rpcAddr, *frequency)
+	prometheus.MustRegister(exporter)
+
+	go exporter.WatchState()
+
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(*addr, nil))
+
 }
